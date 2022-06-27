a8a9a450bbaf5e397735a3dc201cf4bef5a33b01
SteveRSA <stephen.bentley@comprsa.com>
2022-06-27 17:12:20 +0200

    Edit

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,45 +1,57 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { deleteGoal, updateGoal } from '../features/goals/goalSlice'
+import { updateGoal, deleteGoal } from '../features/goals/goalSlice'
+import { FaEdit, FaTimesCircle, FaSave } from 'react-icons/fa'
 
-function GoalItem({ goal }) {
-
-    const [text, setText] = useState('')
+const GoalItem = ({ goal }) => {
+    const [text, setText] = useState(goal.text)
+    const [showEditForm, setShowEditForm] = useState(false)
 
     const dispatch = useDispatch()
 
-    const onSubmit = (e) => {
-        e.preventDefault()
+    const onUpdateGoal = () => {
+        if (text.trim() === '') return
 
-        var id = goal._id;
-        dispatch(updateGoal({ GoalEditID: id, text }));
-        setText('')
+        dispatch(updateGoal({ id: goal._id, text }))
+    }
+
+    const onDeleteGoal = () => {
+        dispatch(deleteGoal(goal._id))
     }
 
     return (
         <div className='goal'>
-            <div>{new Date(goal.createdAt).toLocaleString('en-US')}</div>
-            <h2>{goal.text}</h2>
-            <button onClick={() => dispatch(deleteGoal(goal._id))} className='close'>
-                X
-            </button>
-            <form onSubmit={onSubmit}>
-                <div className='form-group'>
-                    <input
-                        placeholder="Edit Goal"
-                        type='text'
-                        name='text'
-                        id='text'
-                        value={text}
-                        onChange={(e) => setText(e.target.value)}
-                    />
-                </div>
-                <div className='form-group'>
-                    <button className='btn btn-block' type='submit'>
-                        Edit Goal
+            <div>{new Date(goal.updatedAt).toLocaleString('en-US')}</div>
+            {showEditForm ? (
+                <form className='goal-edit-form' onSubmit={e => e.preventDefault()}>
+                    <div className='form-group'>
+                        <input
+                            type='text'
+                            name='text'
+                            id='text'
+                            placeholder='Enter goal text'
+                            value={text}
+                            onChange={e => setText(e.target.value)}
+                        />
+                    </div>
+                    <button type='button' className='goal-save-icon' onClick={onUpdateGoal}>
+                        <FaSave size={25} />
+                    </button>
+                    <button type='button' className='goal-skip-save-icon' onClick={() => setShowEditForm(prevState => !prevState)}>
+                        <FaTimesCircle size={25} />
+                    </button>
+                </form>
+            ) : (
+                <div className='goal-text-wrapper'>
+                    <h2 className='goal-text'>{goal.text}</h2>
+                    <button className='goal-edit-icon' onClick={() => setShowEditForm(prevState => !prevState)}>
+                        <FaEdit size={25} />
                     </button>
                 </div>
-            </form>
+            )}
+            <button className='close' onClick={onDeleteGoal}>
+                X
+            </button>
         </div>
     )
 }
