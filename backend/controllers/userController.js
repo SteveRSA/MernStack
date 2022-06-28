20bf68f90b19763759000ce0260586cadf1b4221
SteveRSA <stephen.bentley@comprsa.com>
2022-06-28 11:17:32 +0200

    Token and Goal Color

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -78,7 +78,7 @@ const getMe = asyncHandler(async (req, res) => {
 // Generate JWT
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '1h',
+        expiresIn: '1d',
     })
 }
 
